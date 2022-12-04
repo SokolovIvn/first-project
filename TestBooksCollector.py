be16e8c6e8f99b89ca4b5250a215d3db235918de
Ivan Sokolov <icanon@ya.ru>
2022-12-04 22:04:05 +0300

    change the names of the test added a new test

--- TestBooksCollector.py
+++ TestBooksCollector.py
@@ -3,25 +3,25 @@ from BooksCollector import BooksCollector
 
 class TestBooksCollector:
 
-    def test_book_name_set_name_check_true(self, book_name):  # добавили новую книгу и проверили имя
+    def test_add_new_book_book_name_true(self, book_name):  # добавили новую книгу и проверили имя
         books = BooksCollector()
         books.add_new_book(name=book_name)
         assert_book = list(books.books_rating.keys())
         assert assert_book[0] == book_name
 
-    def test_book_rating_default_rating_true(self, book_name):  # протестировать дефолтный рейтинг у добавленной книги по имени книги
+    def test_get_book_rating_default_rating_true(self, book_name):  # протестировать дефолтный рейтинг у добавленной книги по имени книги
         books = BooksCollector()
         books.add_new_book(name=book_name)
         assert books.get_book_rating(book_name) == 1
 
-    def test_book_rating_set_custom_rating_true(self, book_name):  # проверка установки кастомного рейтинга
+    def test_get_book_rating_set_custom_rating_true(self, book_name):  # проверка установки кастомного рейтинга
         custom_rating = 3
         books = BooksCollector()
         books.add_new_book(name=book_name)
         books.set_book_rating(book_name, custom_rating)
         assert books.get_book_rating(book_name) == custom_rating
 
-    def test_book_specific_rating_get_some_rating_true(self, book_name):  # проверка вывода книги с определенным рейтингом
+    def test_get_book_specific_rating_set_some_rating_true(self, book_name):  # проверка вывода книги с определенным рейтингом
         custom_rating = 3
         books = BooksCollector()
         books.add_new_book(name=book_name)
@@ -48,10 +48,17 @@ class TestBooksCollector:
         books.add_book_in_favorites(book_name)
         assert book_name in books.get_list_of_favorites_books()
 
-    def test_del_book_in_favorites_check_del_favourite_book_true(self, book_name):  # проверяем, что книга удалена из избранного
+    def test_delete_book_from_favorites_check_del_favourite_book_true(self, book_name):  # проверяем, что книга удалена из избранного
         books = BooksCollector()
         books.add_new_book(name=book_name)
         books.add_book_in_favorites(book_name)
         books.delete_book_from_favorites(book_name)
         aaa = books.get_list_of_favorites_books()
         assert book_name not in aaa
+
+    def test_get_list_of_favorites_books_check_favourite_books_true(self, book_name):  # проверяем список с избранным
+        books = BooksCollector()
+        books.add_new_book(name=book_name)
+        books.add_new_book(name='book_name2')
+        books.add_book_in_favorites(book_name)
+        assert book_name in books.get_list_of_favorites_books()
